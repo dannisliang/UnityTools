252fbaf84b0f3fa95358a754023c221ee27afae2
ageazrael <ageazrael@gmail.com>
2015-04-23 00:56:12 +0800

    AssetRef

---
+++ Assets/AssetRef/UnitTest/AssetRefUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssetRefUnitTest : AssetRefBehaviour
+{
+    public GameObject           OrigGo0;
+    public GameObject           OrigGo1;
+
+    public TAssetRef<GameObject> GameObjectObject;
+    public TAssetRef<Transform> TransformObject;
+    public TAssetRef<Renderer> RendererObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
