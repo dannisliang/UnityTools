c31cde6f2784c186c00d7720fa720374e9b254b7
ageazrael <ageazrael@gmail.com>
2015-04-23 00:53:58 +0800

    AssetRef
    AssetRef 基础实现

---
+++ Assets/AssetRef/AssetRefBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+
+/// <summary>
+/// 
+/// TODO : 如果成员变量名称或者成员顺序发生变化，都可能产生错误
+///     编辑器模式下兼容格式，手机运行使用更加快速的顺序格式。
+/// </summary>
+public class AssetRefBehaviour : MonoBehaviour, ISerializationCallbackReceiver
+{
+    [SerializeField, HideInInspector]
+    private byte[]      mAssetRefData;  
+    void ISerializationCallbackReceiver.OnAfterDeserialize()
+    {
+        if (null == mAssetRefData || mAssetRefData.Length == 0)
+            return;
+
+        using (var rReader = new BinaryReader(new MemoryStream(mAssetRefData)))
+        {
+            // 顺序格式
+            var rBindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+            var rAssetRefType = typeof(AssetRef);
+            foreach (var rField in GetType().GetFields(rBindingFlags))
+            {
+                if (!rField.IsPublic && !rField.IsDefined(typeof(SerializeField), false))
+                    continue;
+
+                if (!rAssetRefType.IsAssignableFrom(rField.FieldType))
+                    continue;
+
+                AssetRef rAssetRef = null;
+                bool bValid = rReader.ReadBoolean();
+                if (bValid)
+                {
+                    rAssetRef = ReflectAssists.Constrct(rField.FieldType) as AssetRef;
+                    rAssetRef.Deserialize(rReader);
+                }
+
+                rField.SetValue(this, rAssetRef);
+            }
+        }
+    }
+    void ISerializationCallbackReceiver.OnBeforeSerialize()
+    {
+        var rMemoryStream = new MemoryStream();
+        using (var rWriter = new BinaryWriter(rMemoryStream))
+        {
+            // 顺序格式
+            var rBindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+            var rAssetRefType = typeof(AssetRef);
+            foreach (var rField in GetType().GetFields(rBindingFlags))
+            {
+                if (!rField.IsPublic && !rField.IsDefined(typeof(SerializeField), false))
+                    continue;
+
+                if (!rAssetRefType.IsAssignableFrom(rField.FieldType))
+                    continue;
+
+                var rAssetRef = rField.GetValue(this) as AssetRef;
+                rWriter.Write(null != rAssetRef ? true : false);
+                if (null != rAssetRef)
+                    rAssetRef.Serialize(rWriter);
+            }
+        }
+        mAssetRefData = rMemoryStream.ToArray();
+    }
+}
---
+++ Assets/AssetRef/Editor/AssetRefInspector.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Reflection;
+using System.Reflection.Emit;
+
+[CustomEditor(typeof(AssetRefBehaviour), true)]
+public class AssetRefInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GUILayout.Label("AssetRef");
+
+        var rAssetRef = this.target as AssetRefBehaviour;
+        var rType = this.target.GetType();
+        foreach (var rField in rType.GetFields())
+        {
+            if (!rField.FieldType.FullName.Contains("AssetRef"))
+                continue;
+
+            if (rField.IsDefined(typeof(HideInInspector), false))
+                continue;
+
+            var rAssetPropInfo  = rField.FieldType.GetProperty("AssetObject");
+            var rAssetType      = rField.FieldType.GetGenericArguments()[0];
+            var rAssetRefObject = rField.GetValue(rAssetRef) as AssetRef;
+            if (null == rAssetRefObject)
+            {
+                rAssetRefObject = ReflectAssists.Constrct(rField.FieldType) as AssetRef;
+                rField.SetValue(rAssetRef, rAssetRefObject);
+            }
+            UnityEngine.Object rNewAssetObject = null;
+
+            if (rAssetRefObject.State == AssetRef.AssetState.None)
+            {
+                rNewAssetObject = EditorGUILayout.ObjectField(rField.Name, null, rAssetType);
+                if (null != rNewAssetObject)
+                    rAssetRefObject.AssetObject = rNewAssetObject;
+            }
+            else if (rAssetRefObject.State == AssetRef.AssetState.Unload)
+            {
+                GUILayout.BeginHorizontal();
+                rNewAssetObject = EditorGUILayout.ObjectField(rField.Name, null, rAssetType);
+                if (null != rNewAssetObject)
+                    rAssetRefObject.AssetObject = rNewAssetObject;
+
+                var rTooltip = string.Format("Load {0}.{1} forme {2} asset.",
+                    rType.Name, rField.Name, rAssetRefObject.AssetName);
+                if (GUILayout.Button(new GUIContent("L", rTooltip), GUILayout.Width(18), GUILayout.Height(14)))
+                    rNewAssetObject = rAssetRefObject.AssetObject;
+                GUILayout.EndHorizontal();
+            }
+            else
+            {
+                rNewAssetObject = EditorGUILayout.ObjectField(rField.Name, 
+                    rAssetRefObject.AssetObject, rAssetType);
+                if (rNewAssetObject != rAssetRefObject.AssetObject)
+                    rAssetRefObject.AssetObject = rNewAssetObject;
+            }
+        }
+    }
+}
